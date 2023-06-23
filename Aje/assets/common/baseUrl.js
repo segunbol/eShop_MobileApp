10de5015af27060548a67d3efa3065a0bd822c78
emmanuel <segunbolawole@yahoo.com>
2023-06-23 18:05:16 +0100

    Test

--- Aje/assets/common/baseUrl.js
+++ Aje/assets/common/baseUrl.js
@@ -10,3 +10,9 @@ let baseURL = "";
 
 export default baseURL;
 //adb reverse tcp:5500 tcp:5500
+// git init
+// git add .
+// git commit -m "Initial commit"
+// git branch -M main
+// git remote add origin https://github.com/segunbol/MyEduGang.git
+// git push -u origin main
